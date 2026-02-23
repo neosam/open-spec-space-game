a31dd4c7812c6b3712fcd3a1848e502539e298c7
Simon Goller <neosam+github@posteo.de>
2026-02-23 20:11:16 +0100

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    openspec.url = "github:Fission-AI/OpenSpec";
+  };
+
+  outputs = { nixpkgs, openspec, ... }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [ openspec.packages.x86_64-linux.default ];
+    };
+  };
+}
